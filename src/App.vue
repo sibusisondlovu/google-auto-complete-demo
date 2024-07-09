4d166d36fff7654ca6c4a8361f4e6a12aaaa0be1
Sibusiso Ndlovu <sibusiso@jaspa.co.za>
2024-07-09 23:03:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import VueGoogleAutocomplete from "vue-google-autocomplete";
+
+export default {
+  components: {VueGoogleAutocomplete},
+
+  data: function () {
+    return {
+      address: "",
+    };
+  },
+  mounted() {
+    // To demonstrate functionality of exposed component functions
+    // Here we make focus on the user input
+    this.$refs.address.focus();
+  },
+  methods: {
+    /**
+     * When the location found
+     * @param {Object} addressData Data of the found location
+     * @param {Object} placeResultData PlaceResult object
+     * @param {String} id Input container ID
+     */
+    getAddressData: function (addressData, placeResultData, id) {
+      this.address = addressData;
+    },
+  }
+};
+</script>
+<template>
+  <div>
+    <h2>Your Address</h2>
+
+    <vue-google-autocomplete
+        ref="address"
+        id="map"
+        classname="form-control"
+        placeholder="Please type your address"
+        v-on:placechanged="getAddressData"
+        country="sg"
+    >
+    </vue-google-autocomplete>
+  </div>
+</template>
+
+<style scoped>
+</style>
